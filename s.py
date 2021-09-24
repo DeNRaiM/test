8e0373ceab6c9f12afbcb834b59f3175fb60c9ba
ИгорьК <kirik.id@phystech.edu>
2021-09-24 11:21:45 +0300

    127

---
+++ s.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+
+p = GPIO.PWM(2, 50) 
+p.start(0)
+try:
+    p.start(0)
+    while True:
+        dc = int(input("введите напряжение в процентах от максимального > "))
+        if dc>=0 and dc<=100:
+            p.ChangeDutyCycle(dc)
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
